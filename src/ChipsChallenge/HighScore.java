2f3dc3044c790b479c10d20902a0515b58aa9a94
Alfie Fitch-May <alfiefitch0@gmail.com>
2023-11-29 17:58:03 +0000

    Added placeholder methods

--- src/ChipsChallenge/HighScore.java
+++ src/ChipsChallenge/HighScore.java
@@ -1,5 +1,19 @@
 package ChipsChallenge;
 
 public class HighScore {
-    // HighScore
+
+    // Return the current highscore
+    public int getCurrentHighscore(){
+        return 1;
+    }
+
+    // Return the player with the current highscore
+    public String getCurrentHighscorePlayer(){
+        return "Temp";
+    }
+    
+    // Check if a new score from a user beats the current highscore.
+    public void uploadNewScore(int score, String user){
+
+    }
 }
